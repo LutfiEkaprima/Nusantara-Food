8e6eb248d5c5de93fc2501f34670a5c391cc83a0
LutfiEka <lutfiekj@gmail.com>
2024-06-23 15:22:14 +0700

    Fix Bug

--- lib/screens/users/home_screen.dart
+++ lib/screens/users/home_screen.dart
@@ -210,12 +210,12 @@ class Section extends StatelessWidget {
                 final recipe = recipes[index];
 
                 final imageUrl =
-                    recipe['imageUrl'] ?? 'https://via.placeholder.com/150';
+                    recipe['imageUrl'] ?? 'https://firebasestorage.googleapis.com/v0/b/nusatara-food.appspot.com/o/default_image%2FIcon.png?alt=media&token=b74c7a3e-950f-402a-9deb-07a0d062be82';
                 final title = recipe['title'] ?? 'No title';
                 final publisherName = recipe['publisherName'] ?? 'Unknown';
                 final rating = recipe['overallRating']?.toString() ?? 'N/A';
 
-                final truncatedPublisherName = publisherName.length > 10
+                final truncatedPublisherName = publisherName.length > 15
                     ? publisherName.substring(0, 15) + '...'
                     : publisherName;
 
