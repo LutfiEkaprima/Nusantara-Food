ff0165fc0d58c4e72463df9c5856dc457461c810
LutfiEka <lutfiekj@gmail.com>
2024-06-30 17:45:45 +0700

    Add Push Notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nusantara_food/onboarding_screen.dart';
@@ -11,11 +12,17 @@ import 'package:nusantara_food/screens/loginmenu.dart';
 import 'package:nusantara_food/screens/reset_password.dart';
 import 'package:nusantara_food/firebase_options.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   runApp(const MyApp());
 }
@@ -31,12 +38,14 @@ class _MyAppState extends State<MyApp> {
   bool _isLoading = true;
   bool _isLoggedIn = false;
   bool _isOnboardingComplete = false;
+  String? _fcmToken;
 
   @override
   void initState() {
     super.initState();
     _checkLoginStatus();
     _checkOnboardingStatus();
+    _setupFirebaseMessaging();
   }
 
   Future<void> _checkLoginStatus() async {
@@ -63,6 +72,37 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  Future<void> _setupFirebaseMessaging() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+
+    // Get the token
+    String? token = await messaging.getToken();
+
+    setState(() {
+      _fcmToken = token;
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      if (message.notification != null) {
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      Navigator.pushNamed(context, '/splash');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     if (_isLoading) {
@@ -80,6 +120,7 @@ class _MyAppState extends State<MyApp> {
           ),
           home: const SplashScreen(),
           routes: {
+            '/splash': (context) => const SplashScreen(),
             '/bottomnav': (context) => const BottomNav(initialIndex: 0, userName: ''),
             '/reset_password': (context) => const ResetPasswordPage(),
             '/home': (context) => const HomeScreen(),
